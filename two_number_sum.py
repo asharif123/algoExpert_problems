045f27278dd0f41fe2b91f7c03a8168e9b6d8bf7
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-07-29 01:04:00 -0700

    Add files via upload

---
+++ two_number_sum.py
@@ -0,0 +1,12 @@
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+# 	do a foor loop of each value and subtract each value from target
+# if the difference is in the list, we know that the number added to sum is in list and the pair equals to target
+# note the values cannot be the same!
+# O(n) time and O(1) space (NOT using any empty arrays or hash tables)	
+	for value in array:
+		difference = targetSum - value
+		if difference in array and difference != value:
+			return [value,difference]
+	return []
+			
